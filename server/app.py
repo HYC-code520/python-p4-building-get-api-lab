467d5244125a7b93b0dfe3f5ddfd974c1889bc6b
Ariel Chen <happilyeverafter0800@gmail.com>
2025-01-30 12:59:36 -0500

    finish lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,49 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    response = make_response (
+            bakeries,
+            200
+    )
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id ==id).first()
+
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        bakery_dict,
+        200
+    )
+
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    prices = [price.to_dict(rules=("-bakeries",)) for price in baked_goods]
+    response = make_response(
+        prices,
+        200
+    )
+    
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+    if baked_goods:
+
+        return make_response(jsonify( baked_goods.to_dict() ),200 )    
+    else:
+        return make_response(jsonify({"error": "No baked goods found"})), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
